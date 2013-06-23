4d6f293c2c220776463182f1a26b60e32a587db0
nihique <kovarik.martin@gmail.com>
2013-06-23 14:45:51 +0200

    breeze sample added

---
+++ BriskIdea/Controllers/BreezeSampleShellController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace BriskIdea.Controllers
+{
+    public class BreezeSampleShellController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BriskIdea/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BriskIdea.Model
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
